030fa74908233b48b62da6ebc02a78c3dd4ef63d
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-09-05 23:44:02 -0700

    added alpha

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,39 @@
+return {
+  "goolord/alpha-nvim",
+  requires = {'BlakeJC94/alpha-nvim-fortune'},
+  config = function ()
+  local alpha = require'alpha'
+  local dashboard = require'alpha.themes.dashboard'
+  local fortune = require'alpha.fortune'
+  dashboard.section.header.val = {
+  [[   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ]],
+  [[    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ]],
+  [[          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ]],
+  [[           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ]],
+  [[          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ]],
+  [[   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ]],
+  [[  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ]],
+  [[ ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ]],
+  [[ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ]],
+  [[      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ]],
+  [[       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
+  }
+  dashboard.section.buttons.val = {}
+
+  dashboard.section.footer.val = fortune()
+
+  vim.api.nvim_create_autocmd("UIEnter", {
+  callback = function()
+  local stats = require("lazy").stats()
+  local time = math.floor(stats.startuptime * 100 + 0.5) / 100
+  dashboard.section.footer.val = { " ", " ", " ", "Neovim loaded " .. stats.count .. " plugins  in " .. time .. "ms" }
+  dashboard.section.footer.opts.hl = "DashboardFooter"
+  end,
+  })
+  -- dashboard.config.opts.noautocmd = true
+  dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
+  dashboard.config.layout[3].val = 5
+  vim.cmd[[autocmd User AlphaReady echo 'ready']]
+  alpha.setup(dashboard.config)
+end
+}
