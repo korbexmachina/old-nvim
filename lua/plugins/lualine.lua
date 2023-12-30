fe72ce7fc33ec25df4f2fca613a30810b90d8b71
Korben Tompkin <contact@korbexmachina.com>
2023-12-30 00:33:05 -0800

    new lualine config to match my tmux config better

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,31 @@
 return {
-      {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'catppuccin',
-        component_separators = '|',
-        section_separators = '',
+  require('lualine').setup {
+    options = {
+      theme = 'catppuccin',
+      component_separators = '|',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = {
+        { 'mode', separator = { left = '', right = '' }, right_padding = 2 },
+      },
+      lualine_b = { 'filename', 'branch', 'diff' },
+      lualine_c = {},
+      lualine_x = { 'fileformat', 'encoding' },
+      lualine_y = { 'filetype', 'progress', 'filesize' },
+      lualine_z = {
+        { 'location', separator = { left = '', right = '' }, left_padding = 2 },
       },
     },
-  },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+  }
 }
